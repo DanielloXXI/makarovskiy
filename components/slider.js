6409c3bc67573796ba515ff452c89af2fc8b52fc
DanielloXXI <cybiran_ez@mail.ru>
2024-04-02 16:18:45 +0300

    feat: сделал слайдер + начал делать страницу отзывов и команды

---
+++ components/slider.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper(".mySwiper", {
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        // renderBullet: function (index, className) {
+        //     return '<span class="' + className + '">' + (index + 1) + "</span>";
+        // },
+    },
+    grabCursor: true,
+    loop: true,
+});
