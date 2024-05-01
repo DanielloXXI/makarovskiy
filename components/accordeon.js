c44989d72d78b5ea8c8fb584b98d742688d84aee
danielloXXI <cybiran_ez@mail.ru>
2024-05-01 16:44:43 +0300

    feat: адаптив страницы "оффлайн курсы"

---
+++ components/accordeon.js
@@ -0,0 +1,36 @@
+const boxes = Array.from(document.querySelectorAll('.section__accordeon-button'));
+const paragraphs = Array.from(document.querySelectorAll('.section__paragraph-box'));
+
+boxes.forEach((box) => {
+    box.addEventListener('click', boxHandler);
+})
+
+function boxHandler(e) {
+    e.preventDefault();
+    let currentBox = e.target.closest('.section__accordeon-button');
+    let currentContent = e.target.previousElementSibling;
+    currentBox.classList.toggle('section__accordeon-button_active');
+    if (currentBox.classList.contains('section__accordeon-button_active')) {
+        currentContent.style.maxHeight = currentContent.scrollHeight + 'px';
+    } else {
+        currentContent.style.maxHeight = 0;
+    }
+}
+
+window.onresize = () => {
+    if (window.innerWidth > 1600) {
+        paragraphs.forEach((paragraph) => {
+            paragraph.style.maxHeight = paragraph.scrollHeight + 'px';
+        })
+        boxes.forEach((box) => {
+            box.classList.add('section__accordeon-button_active');
+        })
+    } else {
+        paragraphs.forEach((paragraph) => {
+            paragraph.style.maxHeight = 0;
+        })
+        boxes.forEach((box) => {
+            box.classList.remove('section__accordeon-button_active');
+        })
+    }
+};
